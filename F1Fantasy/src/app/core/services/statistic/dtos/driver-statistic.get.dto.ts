bd56f3d834113d4ba48542abcf5c352129a9e844
Linh <linh.buitung18@gmail.com>
2025-10-22 23:14:56 +0200

    add driver statistic service

---
+++ F1Fantasy/src/app/core/services/statistic/dtos/driver-statistic.get.dto.ts
@@ -0,0 +1,16 @@
+﻿export interface DriverStatisticGetDto {
+  id: number;
+  givenName: string;
+  familyName: string;
+  code: string;
+  price: number;
+  imgUrl?: string | null;
+}
+
+export interface DriverWithTotalFantasyPointScoredGetDto extends DriverStatisticGetDto {
+  totalFantasyPointScored: number;
+}
+
+export interface DriverWithAverageFantasyPointScoredGetDto extends DriverStatisticGetDto {
+  averageFantasyPointScored: number;
+}
