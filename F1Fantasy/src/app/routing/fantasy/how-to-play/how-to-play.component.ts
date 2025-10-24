ae7595c6a2b0854c64a4d4d38a6bf3813f52704b
Linh <linh.buitung18@gmail.com>
2025-10-24 19:31:57 +0200

    redo and style texts

--- F1Fantasy/src/app/routing/fantasy/how-to-play/how-to-play.component.ts
+++ F1Fantasy/src/app/routing/fantasy/how-to-play/how-to-play.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import {ContentContainerComponent} from "../../../shared/content-container/content-container.component";
+import {CommonModule, NgIf} from '@angular/common';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-how-to-play',
-    imports: [
-        ContentContainerComponent
-    ],
+  imports: [
+    ContentContainerComponent, CommonModule, NgIf, RouterLink
+  ],
   templateUrl: './how-to-play.component.html',
   styleUrl: './how-to-play.component.scss'
 })
 export class HowToPlayComponent {
+  selectedSection: 'build' | 'join' | 'transfers' | 'powerups' | 'race' | 'track' | 'ai' = 'build';
 
+  selectSection(section: typeof this.selectedSection) {
+      this.selectedSection = section;
+    }
 }
--- F1Fantasy/src/app/routing/fantasy/rules/rules.component.ts
+++ F1Fantasy/src/app/routing/fantasy/rules/rules.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
-import {ContentContainerComponent} from "../../../shared/content-container/content-container.component";
+import { ContentContainerComponent } from "../../../shared/content-container/content-container.component";
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-rules',
-    imports: [
-        ContentContainerComponent
-    ],
+  standalone: true,
+  imports: [ContentContainerComponent, CommonModule, NgIf],
   templateUrl: './rules.component.html',
   styleUrl: './rules.component.scss'
 })
 export class RulesComponent {
+  // sections: keep concise keys
+  selectedSection: 'team' | 'transfers' | 'scoring' | 'powerups' | 'leagues' | 'ai' = 'team';
 
+  selectSection(section: typeof this.selectedSection) {
+    this.selectedSection = section;
+  }
 }
